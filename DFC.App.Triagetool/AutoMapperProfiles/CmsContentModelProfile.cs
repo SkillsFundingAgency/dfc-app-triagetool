34e70ed9a769bee84048dc53f939521e4a4a848c
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-04-16 08:54:52 +0100

    Removed Redundant Usings

--- DFC.App.Triagetool/AutoMapperProfiles/CmsContentModelProfile.cs
+++ DFC.App.Triagetool/AutoMapperProfiles/CmsContentModelProfile.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using AutoMapper;
-using DFC.App.Triagetool.Data.Helpers;
-using DFC.App.Triagetool.Data.Models.CmsApiModels;
+﻿using AutoMapper;
 using DFC.App.Triagetool.Data.Models.ContentModels;
+using DFC.App.Triagetool.Models;
 using DFC.App.Triagetool.ViewModels;
-using DFC.Content.Pkg.Netcore.Data.Models;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
+using DFC.Common.SharedContent.Pkg.Netcore.Model.Common;
 using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems;
 using DFC.Common.SharedContent.Pkg.Netcore.Model.Response;
-using DFC.App.Triagetool.Models;
-using DFC.Common.SharedContent.Pkg.Netcore.Model.Common;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DFC.App.Triagetool.AutoMapperProfiles
 {
